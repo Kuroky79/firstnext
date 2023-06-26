45dd2a1dff2e4b6719d030289fb2169751d9a904
firstnextt <alabuzhev.danil@mail.ru>
2023-06-26 18:39:53 +0300

    next

---
+++ src/app/blog/[id]/page.tsx
@@ -0,0 +1,32 @@
+import {Metadata} from "next";
+
+type Props = {
+    params: {
+        id: string
+    }
+}
+async function getData(id: string){
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    {
+        next:{
+
+        }
+    }
+
+    return response.json();
+}
+export async function generateMetadata({params: {id}}: Props): Promise<Metadata>{
+    return {
+        title: id,
+    };
+}
+
+export default async function Post({params: {id}}: Props){
+    const post = await getData(id);
+    return (
+        <>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+        </>
+    )
+}
